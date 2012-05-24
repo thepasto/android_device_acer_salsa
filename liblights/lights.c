1cca901222298a6d6a8ee3332f89d52827bfd0a0
thepasto <thepasto@hotmail.it>
2012-05-24 22:12:00 +0200

    fix leds

--- liblights/lights.c
+++ liblights/lights.c
@@ -15,7 +15,7 @@
  */
 
 
-// #define LOG_NDEBUG 0
+#define LOG_NDEBUG 0
 #define LOG_TAG "lights"
 
 #include <cutils/log.h>
@@ -74,19 +74,9 @@ static int set_light_backlight(struct light_device_t* dev,
 	int fd=open("/dev/avr", O_RDWR);
 	//From lights.h
 	int color=state->color;
-	unsigned char brightness = (state->color)&0xff;//((77*((color>>16)&0x00ff)) + (150*((color>>8)&0x00ff)) + (29*(color&0x00ff))) >> 8;
+	unsigned char brightness = (state->color)&0xff;
 	ioctl(fd, IOCTL_SET_BL_LV, brightness);
 	close(fd);
-	return 0;
-}
-
-static int set_light_keyboard(struct light_device_t* dev,
-		struct light_state_t const* state) {
-	return 0;
-}
-
-static int set_light_buttons(struct light_device_t* dev,
-		struct light_state_t const* state) {
 	return 0;
 }
 
@@ -114,36 +104,37 @@ static int set_light_battery(struct light_device_t* dev,
 		//Off
 		write_int(POWER_LED_FILE, 0);
 	}
-	LOGE("Light battery: %p\n", state->color);
+	LOGE("Light battery: %p %d\n", state->color,state->flashMode);
 	return 0;
 }
 
 static int set_light_notifications(struct light_device_t* dev,
 		struct light_state_t const* state) {
-	if(is_lit(state) && state->flashOnMS) {
+		if(state->color == 0xffffff) {
+			//Notification on
+			//Slow blink
+			LOGE("MAIL WRITE");
 			write_int(MAIL_LED_FILE, 1);
-			write_int("/data/system/mail_led", 1);
-	} else {
+			//write_int(CALL_LED_FILE, 3);
+		} 
+		else if(state->color == 0x00) {
+			//Notification off
+			//Off
+			LOGE("OFF");
 			write_int(MAIL_LED_FILE, 0);
-			write_int("/data/system/mail_led", 0);
-	}
+			write_int(CALL_LED_FILE, 0);
+		} 
+		else { //ANY COLOR 
+			//Notification on
+			//Slow blink
+			//write_int(MAIL_LED_FILE, 0);
+			LOGE("CALL WRITE");
+			write_int(CALL_LED_FILE, 1);
+		}
+
 	LOGE("Notification led: %p(%d,%d,%d)\n", state->color, state->flashMode, state->flashOnMS, state->flashOffMS);
 	return 0;
 }
-
-static int set_light_attention(struct light_device_t* dev,
-		struct light_state_t const* state) {
-	/*if(is_lit(state) && state->flashOnMS) {
-		write_int(BOTT_LED_FILE, 1);
-		write_int(CALL_LED_FILE, 1);
-	} else {
-		write_int(BOTT_LED_FILE, 0);
-		write_int(CALL_LED_FILE, 0);
-	}
-	LOGE("Attention led: %p(%d,%d,%d)\n", state->color, state->flashMode, state->flashOnMS, state->flashOffMS);*/
-	return 0;
-}
-
 
 /** Close the lights device */
 static int close_lights(struct light_device_t *dev) {
@@ -167,17 +158,14 @@ static int open_lights(const struct hw_module_t* module, char const* name,
 			struct light_state_t const* state);
 
 	if (0 == strcmp(LIGHT_ID_BACKLIGHT, name)) {
+		LOGE("backlight");
 		set_light = set_light_backlight;
-	} else if (0 == strcmp(LIGHT_ID_KEYBOARD, name)) {
-		set_light = set_light_keyboard;
-	} else if (0 == strcmp(LIGHT_ID_BUTTONS, name)) {
-		set_light = set_light_buttons;
 	} else if (0 == strcmp(LIGHT_ID_BATTERY, name)) {
+		LOGE("battery");
 		set_light = set_light_battery;
 	} else if (0 == strcmp(LIGHT_ID_NOTIFICATIONS, name)) {
+		LOGE("notification");
 		set_light = set_light_notifications;
-	} else if (0 == strcmp(LIGHT_ID_ATTENTION, name)) {
-		set_light = set_light_attention;
 	} else {
 		return -EINVAL;
 	}
@@ -209,6 +197,6 @@ const struct hw_module_t HAL_MODULE_INFO_SYM = {
 	.version_minor = 0,
 	.id = LIGHTS_HARDWARE_MODULE_ID,
 	.name = "Acer Liquid lights Module",
-	.author = "Pierre-Hugues HUSSON <phhusson@free.fr>",
+	.author = "ThePasto <thepasto@gmail.com>",
 	.methods = &lights_module_methods,
 };
