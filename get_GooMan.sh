a52851318a0bcc153671aa6d9f4c947bb1bfb755
thepasto <thepasto@hotmail.it>
2012-06-23 23:11:57 +0200

    fix gooman script

--- get_GooMan.sh
+++ get_GooMan.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-CUR_VER=$(find device/acer/salsa/proprietary/ -name "GooManager_*")
+CUR_VER=$(find device/acer/salsa/prebuilt/ -name "GooManager_*")
 
 if [[ $CUR_VER == "" ]];then
 	CUR_VER="null"
@@ -10,11 +10,11 @@ AVA_VER=$(curl -s http://goo.im/devs/s0updev/gooinsideme | grep ".apk" | grep hr
 
 if [[ $(basename $AVA_VER) != $(basename $CUR_VER) ]]; then
 	echo -e "     [DOWNLOADING]\n"
-	curl -L $AVA_VER -o "device/acer/salsa/proprietary/"$(basename $AVA_VER)
+	curl -L $AVA_VER -o "device/acer/salsa/prebuilt/"$(basename $AVA_VER)
 	if [ -f $CUR_VER ];then	
 		rm $CUR_VER
 	fi
-	cp -f "device/acer/salsa/proprietary/"$(basename $AVA_VER) "device/acer/salsa/proprietary/GooManager.apk"
+	cp -f "device/acer/salsa/prebuilt/"$(basename $AVA_VER) "device/acer/salsa/prebuilt/GooManager.apk"
 	echo ""
 else
 echo -e "     [UPDATED]\n"
